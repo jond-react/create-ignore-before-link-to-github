bec453cc764acfa31018d3aa56cac97b33ee76d6
jond-react <75000952+jond-react@users.noreply.github.com>
2020-11-25 15:55:03 -0600

    Full index Good fork

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 const { ApolloServer, gql } = require("apollo-server");
+const models = require("./models");
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  type Link {
+    id: Int!
+    url: String!
+    slug: String!
+  }
+
   type Query {
-    hello: String
+    getLink(id: Int!): Link!
+    getLinks: [Link!]!
+  }
+
+  type Mutation {
+    createLink(url: String!, slug: String!): Link!
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: (root, args, context) => "Hello world!"
+    async getLinks(root, args, { models }) {
+      return models.Link.findAll();
+    },
+    async getLink(root, { id }, { models }) {
+      return models.Link.findByPk(id);
+    }
+  },
+  Mutation: {
+    async createLink(root, { url, slug }, { models }) {
+      return models.Link.create({
+        url,
+        slug
+      });
+    }
   }
 };
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: { models }
 });
 
 server.listen().then(({ url }) => {
